f6d4a46dbb4836599209719437f6642443a87bb0
shah4571 <shahriya4571@gmail.com>
2025-08-05 23:20:19 +0600

    Delete bot directory

--- bot/handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import start
-from . import cap
-from . import account
-from . import withdraw
-from . import support
-from . import admin
--- bot/utils/converter.py
+++
@@ -1,4 +0,0 @@
-from bot.config import TRX_RATE
-
-def usd_to_trx(usd_amount):
-    return round(float(usd_amount) * TRX_RATE, 2)
