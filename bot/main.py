06c7852389e7ab3842ef99ff49c937718dfd7540
shah4571 <shahriya4571@gmail.com>
2025-08-05 16:55:01 +0600

    main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,5 @@
-
 from pyrogram import Client, filters
-from config import BOT_TOKEN, API_ID, API_HASH, ADMIN_ID
+
 from bot.handlers import start, cap, account, withdraw, support, admin
 
 app = Client("session_bot", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH)
