0c07435ab078953438e5d3d2768f16d5ac85b067
shah4571 <shahriya4571@gmail.com>
2025-08-05 18:28:38 +0600

    support.py

--- bot/handlers/support.py
+++ bot/handlers/support.py
@@ -1,5 +1,10 @@
 
 from pyrogram.types import Message
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("support") & filters.private)
 async def support_handler(client, message: Message):
-    await message.reply("🆘 For help contact: @xrd_didox")
+    await message.reply_text(
+        "🆘 For help contact: @xrd_didox",
+        quote=True
+    )
